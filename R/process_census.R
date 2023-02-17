dc84da7cb85031608af2477304bbb113cd30fe5e
neilcharles <neil.d.charles@gmail.com>
2023-02-17 15:20:45 +0000

    reset commit

---
+++ R/process_census.R
@@ -0,0 +1,155 @@
+process_facts <- function(){
+
+  var_names <- readr::read_csv("inst/extdata/stats/2011/name_match.csv") %>%
+    dplyr::mutate(scot_name = janitor::make_clean_names(scot_name),
+                  eng_wa_name = janitor::make_clean_names(eng_wa_name),
+                  name = janitor::make_clean_names(name)) %>%
+    dplyr::filter(include) %>%
+    dplyr::select(-include)
+
+  # Load England & Wales ---------------------------------------------------------
+  raw_ew <- tibble::tibble(filename = list.files(path = "inst/extdata/stats/2011/england_wales", pattern = ".*.csv", full.names = FALSE)) %>%
+    dplyr::mutate(data = purrr::map(.x = filename, .f = ~ readr::read_csv(glue::glue("inst/extdata/stats/2011/england_wales/{.x}"), col_types = cols(.default = "c")))) %>%
+    dplyr::mutate(filename = stringr::str_extract(filename, "^[A-Z][A-Z]\\d\\d\\d")) %>%
+    dplyr::filter(filename %in% var_names$filename)
+
+  raw_ew_long <- raw_ew %>%
+    dplyr::mutate(data2 = map(
+      .x = data,
+      .f = ~
+        tidyr::pivot_longer(
+          janitor::clean_names(.),
+          -c(date, geography, `geography_code`, `rural_urban`)
+        )
+    )) %>%
+    dplyr::select(-data) %>%
+    tidyr::unnest(data2) %>%
+    dplyr::select(-date, -geography, -rural_urban) %>%
+    dplyr::rename(geo_id = geography_code,
+                  eng_wa_name = name) %>%
+    dplyr::left_join(select(var_names, filename, eng_wa_name, name)) %>%
+    dplyr::select(-eng_wa_name)
+
+  # Load Scotland ----------------------------------------------------------------
+  raw_scot <- tibble::tibble(filename = list.files(path = "inst/extdata/stats/2011/scotland/KS", pattern = "*.csv", full.names = FALSE)) %>%
+    dplyr::mutate(data = purrr::map(.x = filename, .f = ~readr::read_csv(glue::glue("inst/extdata/stats/2011/scotland/KS/{.x}"), col_types = cols(.default = "c")) %>%
+                                      dplyr::rename(geography_code = ...1))) %>%
+    dplyr::mutate(filename = stringr::str_extract(filename, "^[A-Z][A-Z]\\d\\d\\d")) %>%
+    dplyr::filter(filename %in% var_names$filename)
+
+  raw_scot_long <- raw_scot %>%
+    dplyr::mutate(data2 = map(
+      .x = data,
+      .f = ~
+        tidyr::pivot_longer(
+          janitor::clean_names(.),
+          -c(`geography_code`)
+        )
+    )) %>%
+    dplyr::select(-data) %>%
+    tidyr::unnest(data2) %>%
+    dplyr::rename(geo_id = geography_code,
+                  scot_name = name) %>%
+    dplyr::left_join(select(var_names, filename, scot_name, name)) %>%
+    dplyr::select(-scot_name)
+
+
+  # Merge England, Wales & Scotland
+  raw_all <- raw_ew_long %>%
+    dplyr::union_all(raw_scot_long) %>%
+    dplyr::filter(glue::glue("{filename}{name}") %in% glue::glue("{var_names$filename}{var_names$name}")) %>% #Remove unwanted vars
+    dplyr::select(-filename) %>%
+    dplyr::mutate(value = stringr::str_replace(value, "^-$", "0")) %>%
+    dplyr::mutate(value = stringr::str_replace_all(value, ",","")) %>%
+    dplyr::mutate(value = as.numeric(value))
+
+  raw_wide <- raw_all %>%
+    tidyr::pivot_wider(names_from = "name", values_from = "value")
+
+  # check for NA and identify problem columns
+  if(any(is.na(raw_wide))){
+
+    for(i in 1:ncol(raw_wide)){
+      if(any(is.na(raw_wide[,i]))) print(i)
+    }
+
+    stop("Names didn't match")
+  }
+
+  facts_2011 <- raw_wide
+
+  usethis::use_data(facts_2011, overwrite = TRUE)
+
+  # Run this if output contains list cols
+  # test <- raw_all %>%
+  #   dplyr::group_by(geo_id, name) %>%
+  #   dplyr::summarise(n = dplyr::n(), .groups = "drop") %>%
+  #   dplyr::filter(n > 1L)
+
+}
+
+create_segmentation <- function(){
+
+  #Convert to % of population except for density columns
+  pct_all <- facts_2011 %>%
+    mutate(across(!geo_id &
+                    !variable_all_usual_residents &
+                    !variable_density_number_of_persons_per_hectare,
+                  ~./variable_all_usual_residents))
+
+  segmentation_raw <- scale(
+    pct_all[3:ncol(pct_all)]
+  )
+
+  set.seed(42)
+  fit <- kmeans(segmentation_raw, 12, algorithm = "Hartigan", iter.max = 1000)
+
+  # tibble(x = fit$cluster) %>%
+  #   group_by(x) %>%
+  #   summarise(n = n()) %>%
+  #   ggplot2::ggplot(aes(x = x, y = n)) +
+  #   geom_col()
+
+  mapdata <- tibble(geo_id = pct_all$geo_id,
+                    cluster = fit$cluster)
+
+  colours <- tibble(col = pals::alphabet()) %>%
+    mutate(cluster=row_number())
+
+  mapshapes <- rUKcensus::shapes_lsoa %>%
+    left_join(mapdata) %>%
+    left_join(colours)
+
+  # leaflet(filter(mapshapes, cluster==10)) %>%
+  # leaflet::leaflet(mapshapes) %>%
+  #   leaflet::addProviderTiles(leaflet::providers$CartoDB.Positron) %>%
+  #   leaflet::addPolygons(fillColor = ~as.character(col),
+  #               popup = ~as.character(cluster),
+  #               weight = 0.5,
+  #               color = "black")
+
+cluster_names <- tibble::tibble(
+  cluster = seq(1:12),
+  cluster_name = c(
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "")
+)
+
+clusters_lsoa_2011 <- mapdata %>%
+  sf::st_drop_geometry() %>%
+  left_join(cluster_names)
+
+
+usethis::use_data(clusters_lsoa_2011, overwrite = TRUE)
+
+}
---
+++ R/process_shapes.R
@@ -0,0 +1,56 @@
+build_shapes <- function() {
+
+  lookup_areas <- readr::read_csv("inst/extdata/lookup/area_names/PCD_OA_LSOA_MSOA_LAD_MAY21_UK_LU.csv") %>% #not in package because huge
+    dplyr::mutate(post_town = stringr::str_extract(pcd7, "^\\D+")) %>%
+    dplyr::group_by(lsoa11cd, msoa11cd, ladcd, lsoa11nm, msoa11nm, ladnm) %>%
+    dplyr::summarise() %>%
+    dplyr::ungroup()
+
+  usethis::use_data(lookup_areas, overwrite = TRUE)
+
+  shapes_lsoa <-
+    sf::st_read("inst/extdata/shapes/lsoa/lsoa.shp") %>%
+    sf::st_transform(crs = 4326) %>%
+    dplyr::rename(geo_id = LSOA11CD,
+                  geo_name = LSOA11NM) %>%
+    dplyr::select(geo_id, geo_name, geometry)
+
+  shapes_lsoa_scot <-
+    sf::st_read("inst/extdata/shapes/lsoa/output-area-2011-mhw/OutputArea2011_MHW.shp") %>%
+    sf::st_transform(crs = 4326) %>%
+    dplyr::rename(geo_id = code) %>%
+    mutate(geo_name = "") %>%
+    dplyr::select(geo_id, geo_name, geometry)
+
+  shapes_lsoa <- shapes_lsoa %>%
+    union_all(shapes_lsoa_scot)
+
+  shapes_lad <-
+    sf::st_read("inst/extdata/shapes/la/LAD_DEC_2021_UK_BGC.shp") %>%
+    sf::st_transform(crs = 4326) %>%
+    dplyr::rename(geo_id = LAD21CD,
+                  geo_name = LAD21NM) %>%
+    dplyr::select(geo_id, geo_name, geometry) %>%
+    dplyr::left_join(
+      readr::read_rds("inst/extdata/stats/population/stats_population_lad.rds"), by = "geo_id"
+    )
+
+
+  shapes_msoa <-
+    sf::st_read("inst/extdata/shapes/msoa/msoa.shp") %>%
+    sf::st_transform(crs = 4326) %>%
+    dplyr::rename(geo_id = MSOA11CD,
+                  geo_name = MSOA11NM) %>%
+    dplyr::select(geo_id, geo_name, geometry) %>%
+    dplyr::left_join(
+      readr::read_rds("inst/extdata/stats/population/stats_population_msoa.rds"), by = "geo_id"
+    )
+
+  shapes_uk_coastline <- shapes_msoa %>%
+    dplyr::summarise(geometry = sf::st_union(geometry))
+
+  usethis::use_data(shapes_lad,
+                    shapes_msoa,
+                    shapes_lsoa,
+                    shapes_uk_coastline, overwrite = TRUE)
+}
